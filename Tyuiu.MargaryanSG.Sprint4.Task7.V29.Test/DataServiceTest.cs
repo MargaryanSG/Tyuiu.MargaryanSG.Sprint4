1f41b68a8229afeda6ef66c12514bc404ffaee1d
Tyuiu.MargaryanSG@outlook.com <Tyuiu.MargaryanSG@outlook.com>
2023-11-12 20:05:24 +0500

    Task7

---
+++ Tyuiu.MargaryanSG.Sprint4.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MargaryanSG.Sprint4.Task7.V29.Lib;
+namespace Tyuiu.MargaryanSG.Sprint4.Task7.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void FindCountsInString()
+        {
+            DataService DataService = new DataService();
+            int n = 4;
+            int m = 3;
+            string value = "983157642891";
+            int sum = 28;
+            int calc = DataService.Calculate(n, m, value);
+            Assert.AreEqual(calc, sum);
+        }
+    }
+}
---
+++ Tyuiu.MargaryanSG.Sprint4.Task7.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MargaryanSG.Sprint4.Task7.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MargaryanSG.Sprint4.Task7.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("102a6c60-4c34-448c-95bf-6fd1e05b8257")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
